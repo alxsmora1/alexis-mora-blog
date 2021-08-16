6650bfc784d68429f404af669b536d506ba42852
ALXSMORA\Alexis Mora <alexis.mora1@icloud.com>
2021-08-15 21:47:17 -0500

    Create a link to my protfolio in the menu

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,7 +3,8 @@ import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
-  <nav className="navigation"> 
+  <nav className="navigation">
+    <a target="_blank"  rel="noopener noreferrer" href="https://alxsmora1.github.io/portfolio/#/">Portfolio</a>
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
