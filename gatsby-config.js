65889501e8c607b730e90db9f42c071f116d06e9
Alexis Mora <alexis.mora1v@gmail.com>
2022-01-25 19:26:46 -0600

    Add Google Analytics ID fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "G-8VBCQNV7FV",
+        trackingId: "UA-300832296-1",
         head: true
       }
     },
