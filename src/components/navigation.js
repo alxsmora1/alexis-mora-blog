24a7d4debb87026728654490c1ecc9268bb0cb82
Alexis Mora <alexis.mora1v@gmail.com>
2022-10-06 02:45:50 -0500

    Add portfolio link

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,13 +4,7 @@ import ThemeChanger from "../components/themeChanger";
 
 export default props => (
   <nav className="navigation">
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="https://alxsmora1.github.io/portfolio/#/"
-    >
-      Portfolio
-    </a>
+    <Link to="/portfolio">Portfolio</Link>
     <Link to="/contact">Contact</Link>
     <ThemeChanger />
   </nav>
